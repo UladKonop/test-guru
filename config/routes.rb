1de87a1fdb16ab08330d059185fcd7681ceacd40
uladkonop <vladknpljov@gmail.com>
2021-09-13 22:07:23 +0300

    added about, author static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/about', :to => redirect('/about.html')
+  get '/about/author', :to => redirect('/author.html')
 end
