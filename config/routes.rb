04195e604e8e91e69408aea235b93ffef969b685
uladkonop <vladknpljov@gmail.com>
2021-09-14 20:27:18 +0300

    remove unnessesary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/about', :to => redirect('/about.html')
-  get '/about/author', :to => redirect('/author.html')
 end
