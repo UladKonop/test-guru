52b455888ba24cf5d9c9455bcdb11e3a4e8597f2
uladkonop <vladknpljov@gmail.com>
2021-09-14 17:26:53 +0300

    add version initializer

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,13 @@
 # frozen_string_literal: true
 
-VERSION = SemVer.find
+module Version
+  extend TestGuru
+
+  def TestGuru.version
+    @version ||= '0.0.0'
+  end
+
+  def TestGuru.version=(str)
+    @version = str
+  end
+end
