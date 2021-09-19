c6c4a6400ab462d45a0082b9856e0b247ee38560
uladkonop <vladknpljov@gmail.com>
2021-09-19 20:22:13 +0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'Foo')
+User.create!(name: 'Bar')
+
+category = Category.create!(title: 'frontend')
+category1 = Category.create!(title: 'backend')
+category2 = Category.create!(title: 'OS')
+
+test = Test.create!(title: 'JS', category_id: category.id)
+test1 = Test.create!(title: 'Ruby', category_id: category1.id)
+test2 = Test.create!(title: 'Linux', category_id: category2.id)
+test3 = Test.create!(title: 'JS', level: 1, category_id: category.id)
+test4 = Test.create!(title: 'Windows', level: 2, category_id: category2.id)
+test5 = Test.create!(title: 'Linux', level: 3, category_id: category2.id)
+
+question = Question.create!(body: 'Is the JS language related to the java language?', test_id: test.id)
+question1 = Question.create!(body: 'What is the name of libraries in the Ruby language?', test_id: test1.id)
+question2 = Question.create!(body: 'What family of operating systems is linux?', test_id: test2.id)
+question3 = Question.create!(body: 'Is JS compiled or interpreted?', test_id: test3.id)
+question4 = Question.create!(body: 'How to get to the command line?', test_id: test4.id)
+question5 = Question.create!(body: 'How to find out the username in the console?', test_id: test5.id)
+
+Answer.create!(body: 'No.', question_id: question.id)
+Answer.create!(body: 'Gem(s).', question_id: question1.id)
+Answer.create!(body: 'Unix.', question_id: question2.id)
+Answer.create!(body: 'Interpreted.', question_id: question3.id)
+Answer.create!(body: 'CTR+T.', question_id: question4.id)
+Answer.create!(body: 'whoami', question_id: question5.id)
