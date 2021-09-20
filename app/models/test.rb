215aa28d66fca5b70efebbf4077270fa64567c9f
uladkonop <vladknpljov@gmail.com>
2021-09-20 11:39:14 +0300

    added class method test_titles

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.test_titles(category)
+    Test.joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
+  end
 end
