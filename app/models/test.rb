8bee590252c8ff610d417632f223cf68d67788be
uladkonop <vladknpljov@gmail.com>
2021-09-22 12:40:04 +0300

    implemented many-to-many assotiation for user-test

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,10 @@
 
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :users
+
+  has_many :user_tests
+  has_many :users, through: :user_tests
 
   def self.test_titles(category)
     Test.joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
