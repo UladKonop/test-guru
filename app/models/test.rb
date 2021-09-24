8a6e88930b8d3a96a13c092e114c24a508e38241
uladkonop <vladknpljov@gmail.com>
2021-09-24 22:30:39 +0300

    added assotiations to models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+
+  has_many :user_tests
+  has_many :users, through: :user_tests
+
+  def self.test_titles(category)
+    Test.joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
+  end
 end
