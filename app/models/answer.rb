2ca05512dc6bd515003d6b52fb9ea01bc840e67e
uladkonop <vladknpljov@gmail.com>
2021-09-28 17:28:21 +0300

    added max_answers_amount validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,4 +6,11 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true, uniqueness: true
+  validates :max_answers_amount, on: :create
+
+  private
+
+  def max_answers_amount
+    errors.add(:question) if question.answers.size > 4
+  end
 end
