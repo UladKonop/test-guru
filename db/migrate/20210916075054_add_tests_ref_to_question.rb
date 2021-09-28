9e4e44f82e5ed58071f4facf3ad565aa51058bf8
Uladzislau Kanapliou <45392197+UladKonop@users.noreply.github.com>
2021-09-28 10:29:07 +0300

    added assotiations to models (#5)
    * added assotiations to models

--- db/migrate/20210916075054_add_tests_ref_to_question.rb
+++ db/migrate/20210916075054_add_tests_ref_to_question.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTestsRefToQuestion < ActiveRecord::Migration[6.1]
   def change
     add_reference :questions, :test, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_084548) do
+ActiveRecord::Schema.define(version: 2021_09_27_174807) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2021_09_22_084548) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -62,4 +64,5 @@ ActiveRecord::Schema.define(version: 2021_09_22_084548) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users", column: "author_id"
 end
