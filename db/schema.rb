f756925630c923409d6204bc5e6399ef78c53601
uladkonop <vladknpljov@gmail.com>
2021-09-28 17:10:17 +0300

    added email field to User, fixed seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_174807) do
+ActiveRecord::Schema.define(version: 2021_09_28_140252) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_174807) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
   end
 
   add_foreign_key "answers", "questions"
