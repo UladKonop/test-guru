31fec26cd695b3da00d62a77888ffecec07555e8
uladkonop <vladknpljov@gmail.com>
2021-10-17 15:19:45 +0300

    added CRUD methods

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,27 +1,43 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show destroy]
+  before_action :find_test, except: %i[index new create]
 
   def index
     @tests = Test.all
   end
 
-  def show
-    @test = Test.find(params[:id])
-  end
+  def show; end
 
   def new; end
 
   def create
-    test = Test.create(test_params)
-    render plain: test.inspect
+    test = Test.new(test_params)
+
+    if test.save
+      redirect_to tests_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to tests_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
   end
 
   private
 
   def test_params
-    params.require(:test).permit(:title, :level)
+    params.require(:test).permit(:title, :level, :category_id)
   end
 
   def find_test
