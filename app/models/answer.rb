d146c787a61bf0a67bf1371a979a9f914da23334
uladkonop <vladknpljov@gmail.com>
2021-10-26 15:39:57 +0300

    add validation for correctness

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,11 +6,12 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true
-  validate :max_answers_amount, on: :create
+  validates :correct, presence: true
+  validate :validate_answers_amount, on: :create
 
   private
 
-  def max_answers_amount
+  def validate_answers_amount
     errors.add(:question) if question.answers.size > 4
   end
 end
