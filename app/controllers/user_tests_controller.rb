726c8b9db5b7c41d177c2ba0723f4778b70ea0d4
uladkonop <vladknpljov@gmail.com>
2021-10-26 20:00:30 +0300

    add before action, update method

--- app/controllers/user_tests_controller.rb
+++ app/controllers/user_tests_controller.rb
@@ -1,9 +1,25 @@
 # frozen_string_literal: true
 
 class UserTestsController < ApplicationController
+  before_action :set_user_test, only: %i[show result update]
+
   def show; end
 
   def result; end
 
-  def update; end
+  def update
+    @user_test.accept!(params[:answer_ids])
+
+    if @user_test.completed?
+      redirect_to result_user_test_path(@user_test)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_user_test
+    @user_test = UserTest.find(params[:id])
+  end
 end
