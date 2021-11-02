2b7a89ddc0b4ccc406baf886ba9e86035e72a034
uladkonop <vladknpljov@gmail.com>
2021-11-02 21:44:11 +0300

    use 1 migrations i.o. 3

--- db/migrate/20211025085106_add_correct_questions_to_user_test.rb
+++ db/migrate/20211025085106_add_correct_questions_to_user_test.rb
@@ -3,5 +3,7 @@
 class AddCorrectQuestionsToUserTest < ActiveRecord::Migration[6.1]
   def change
     add_column :user_tests, :correct_questions, :integer
+    add_reference :user_tests, :question, foreign_key: true
+    change_column_default :user_tests, :correct_questions, from: nil, to: 0
   end
 end
--- db/migrate/20211025085610_add_question_ref_to_user_test.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddQuestionRefToUserTest < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :user_tests, :question, foreign_key: true
-  end
-end
