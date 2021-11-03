619dc0b58bb45e5fe4194e5ad7b8616f193ecc51
uladkonop <vladknpljov@gmail.com>
2021-11-03 11:15:35 +0300

    add digest method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :user_tests
   has_many :tests, through: :user_tests
@@ -19,5 +21,11 @@ class User < ApplicationRecord
 
   def authenticate(password_string)
     digest(password_string) == password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
