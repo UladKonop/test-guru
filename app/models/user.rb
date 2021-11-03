308a893ab6375bd66094044c548d1cfa591d99dd
uladkonop <vladknpljov@gmail.com>
2021-11-03 14:18:55 +0300

    add auth module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
-require 'digest/sha1'
-
 class User < ApplicationRecord
+  include Auth
+
   has_many :user_tests
   has_many :tests, through: :user_tests
 
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  has_secure_password
 
   def tests_sort_by_level(level)
     tests.level(level)
@@ -17,15 +16,5 @@ class User < ApplicationRecord
 
   def user_test(test)
     user_tests.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(password_string)
-    digest(password_string) == password_digest ? self : false
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
