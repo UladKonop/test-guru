5935d55fe56fd12a16051208ca1f48061ec09e0b
uladkonop <vladknpljov@gmail.com>
2021-11-06 17:55:10 +0300

    add uniqueness email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def tests_sort_by_level(level)
     tests.level(level)
