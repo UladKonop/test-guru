94de280942d099910ed1d5666df091392f4a75af
uladkonop <vladknpljov@gmail.com>
2021-11-06 17:31:44 +0300

    add destroy method, add after_act. to set user_id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,31 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
-  end
+  before_action :set_user, only: :create
+  after_action :set_user_id, only: :create
+
+  def new; end
 
   def create
-    user = User.find_by(email: params[:email])
-
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
+    if @user&.authenticate(params[:password])
       redirect_to tests_path
     else
       render :new
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def set_user_id
+    session[:user_id] = @user.id
   end
 end
