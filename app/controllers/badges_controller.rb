c5aaab67e868dd416492f80669d1c3d24cb711f3
uladkonop <vladknpljov@gmail.com>
2022-02-17 17:48:51 +0300

    add user badge controller

---
+++ app/controllers/badges_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class BadgesController < ApplicationController
+  def index
+    @badges = Badge.all
+    @user_badges = current_user.badges
+  end
+
+  def show
+    @badge = Badge.find(params[:id])
+  end
+end
